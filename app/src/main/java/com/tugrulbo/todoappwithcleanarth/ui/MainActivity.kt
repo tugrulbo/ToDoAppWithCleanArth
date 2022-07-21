a707facb076d92e02521913ac903c65bdd3921d2
tugrulb <tugrulbo@gmail.com>
2022-07-21 06:26:14 +0300

    Add Fragment Done

--- app/src/main/java/com/tugrulbo/todoappwithcleanarth/ui/MainActivity.kt
+++ app/src/main/java/com/tugrulbo/todoappwithcleanarth/ui/MainActivity.kt
@@ -2,11 +2,35 @@ package com.tugrulbo.todoappwithcleanarth.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.tugrulbo.todoappwithcleanarth.R
+import com.tugrulbo.todoappwithcleanarth.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = getNavController()
+        setupActionBarWithNavController(navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = getNavController()
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    private fun getNavController(): NavController {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+
+        return navHostFragment.navController
     }
 }
