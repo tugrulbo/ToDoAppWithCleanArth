61d7fe57ff5fa8c851b7fd736d3a81ae82c983c4
tugrulb <tugrulbo@gmail.com>
2022-07-21 05:50:07 +0300

    Navigation done

--- app/src/main/java/com/tugrulbo/todoappwithcleanarth/ui/MainActivity.kt
+++ app/src/main/java/com/tugrulbo/todoappwithcleanarth/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.tugrulbo.todoappwithcleanarth
+package com.tugrulbo.todoappwithcleanarth.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tugrulbo.todoappwithcleanarth.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
